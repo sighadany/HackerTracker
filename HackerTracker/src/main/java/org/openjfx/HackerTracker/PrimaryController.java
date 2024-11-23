561b398e1a6a0ca41e1a72d405e7d08a5a9e6bef
Dany <nohonetsigha.d@northeastern.edu>
2024-11-22 23:01:53 -0500

    made calendar wider; added date; added question buttons

--- HackerTracker/src/main/java/org/openjfx/HackerTracker/PrimaryController.java
+++ HackerTracker/src/main/java/org/openjfx/HackerTracker/PrimaryController.java
@@ -2,6 +2,11 @@ package org.openjfx.HackerTracker;
 
 import java.io.IOException;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
@@ -9,9 +14,14 @@ import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
+	
+	private VBox selectedDay;
+	private int questionIndex;
 
     @FXML
     private void switchToSettings() throws IOException {
@@ -28,15 +38,89 @@ public class PrimaryController {
     
     @FXML
     private Spinner<Integer> timeSpinner;
+    
+    @FXML
+    private VBox Monday;
+
+    @FXML
+    private VBox Tuesday;
+    
+    @FXML
+    private VBox Wednesday;
+    
+    @FXML
+    private VBox Thursday;
+    
+    @FXML
+    private VBox Friday;
+    
+    @FXML
+    private VBox Saturday;
+    
+    @FXML
+    private VBox Sunday;
+    
+    @FXML 
+    private Text fullDate;
+    
+    @FXML
+    public void addQuestion() {
+        if (selectedDay != null) {
+            Button newButton = new Button("Q. " + questionIndex);
+            newButton.setWrapText(true);
+            selectedDay.getChildren().add(newButton);
+        }
+    }
+    
+    @FXML
+    public void setDate() {
+    	LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy");
+        String formattedDate = currentDate.format(formatter);
+    	fullDate.setText(formattedDate);
+    }
+    
+    
+    public void setDayAndIndex(VBox day, int i) {
+        this.selectedDay = day;
+        this.questionIndex = i;
+    }
+    
 
     @FXML
     public void initialize() {
+    	setDate();
+    	
         ObservableList<String> options = FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
         difficultyChoiceBox.setItems(options);
         
      // Define the range and step size
         SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 180, 30, 1);
         timeSpinner.setValueFactory(valueFactory);
+     
+        
+     // for now we manually enter the number of questions
+        
+        for(int i = 1; i <= 3; i++) {
+        	setDayAndIndex(Monday, i);
+        	addQuestion();
+        }
+        
+        for(int i = 1; i <= 6; i++) {
+        	setDayAndIndex(Wednesday, i);
+        	addQuestion();
+        }
+        
+        for(int i = 1; i <= 2; i++) {
+        	setDayAndIndex(Sunday, i);
+        	addQuestion();
+        }
+        
     }
+    
+    
+    
+    
+    
     
 }
