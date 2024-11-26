4429acdcf71209c0a9ffd1e800918300750effd5
Sarthak Mallick <sarthak4242@gmail.com>
2024-11-26 13:13:42 -0500

    question title testing

--- HackerTracker/src/main/java/module-info.java
+++ HackerTracker/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module org.openjfx.HackerTracker {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+	requires org.mongodb.driver.sync.client;
+	requires org.mongodb.bson;
+	requires javafx.base;
 
     opens org.openjfx.HackerTracker to javafx.fxml;
     exports org.openjfx.HackerTracker;
--- HackerTracker/src/main/java/org/openjfx/HackerTracker/PrimaryController.java
+++ HackerTracker/src/main/java/org/openjfx/HackerTracker/PrimaryController.java
@@ -2,11 +2,22 @@ package org.openjfx.HackerTracker;
 
 import java.io.IOException;
 
+//import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.collections.FXCollections;
 
 import javafx.scene.input.MouseEvent;
@@ -31,8 +42,18 @@ public class PrimaryController {
     @FXML
     private Spinner<Integer> timeSpinner;
     
+    @FXML 
+    private int numberOfMondayButtons;
     @FXML
     private VBox mondayButtons;
+    
+    @FXML 
+    private int numberOfTuesdayButtons;
+    @FXML 
+    private VBox tuesdayButtons;
+    
+    @FXML
+    private Label questionTitle;
 
     public void test(){
         System.out.println("Testing");
@@ -47,11 +68,33 @@ public class PrimaryController {
         SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 180, 30, 1);
         timeSpinner.setValueFactory(valueFactory);
         
-        Button Q1 = new Button("Q1");
-        mondayButtons.getChildren().add(Q1);
+        numberOfMondayButtons = 5;
+        for(int i=1; i<=numberOfMondayButtons; i++) {
+        	Button problemTile = new Button("Q"+i);
+        	problemTile.setOnAction(new ShowProblemDetails());
+            mondayButtons.getChildren().add(problemTile);
+        }
         
-        Button Q2 = new Button("Q1");
-        mondayButtons.getChildren().add(Q2);
+        numberOfTuesdayButtons = 3;
+        for(int i=1; i<=numberOfTuesdayButtons; i++) {
+        	tuesdayButtons.getChildren().add(new Button("Q"+i));
+        }
+        
+//        MongoClient client = new MongoClient("mongodb+srv://admin:admin@hackertracker.1ovmp.mongodb.net/?retryWrites=true&w=majority&appName=HackerTracker");
+//        MongoDatabase db = client.getDatabase("HackerTracker");
+//        MongoCollection<Document> problems = db.getCollection("problems");
+        MongoClient client = MongoClients.create("mongodb+srv://admin:admin@hackertracker.1ovmp.mongodb.net/?retryWrites=true&w=majority&appName=HackerTracker");
+        MongoDatabase db = client.getDatabase("HackerTracker");
+        MongoCollection<Document> problems = db.getCollection("problems");
+        Document doc = problems.find().first();
+        System.out.println(doc.toJson());
+    }
+    
+    class ShowProblemDetails implements EventHandler<ActionEvent>{
+    	@Override
+    	public void handle(ActionEvent event) {
+    		questionTitle.setText("Problem title 1");
+    	}
     }
     
 }
